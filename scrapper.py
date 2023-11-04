78b551626e23dcd7615abc7e2e2819f67a912aa8
MichalPrzyl <michal.przyl@gmail.com>
2023-11-04 14:34:19 +0100

    messing around

--- scrapper.py
+++ scrapper.py
@@ -1,31 +1,30 @@
-import requests
-from utils import format_price, color
 
-COLUMN_WIDTH = 30
+elements = [
+    {
+        'name': "Hello123123123",
+        "price": '33,23 zł'
+    },
+    {
+        'name': "second",
+        "price": '554,22 zł'
+    },
+    {
+        'name': "lol3",
+        "price": '672,91 zł'
+    },
+]
+
+najdluzszy_name_length = max(len(d["name"]) for d in elements)
+WIDTH_VARIABLE = najdluzszy_name_length
+
+# print("| {{:<{}s}} | {:<50s} |".format(WIDTH_VARIABLE).format("NAME", "PRICE"))
+# print(60 *"-")
 
 
-url = "http://store.steampowered.com/api/featuredcategories/?l=polish"
+# print("| {{:<{}s}} | {:6.2f} |".format(WIDTH_VARIABLE).format("Bill", "123"))
+# print("| {{:<{}s}} | {:6.2f} |".format(WIDTH_VARIABLE).format("Bill", "semanko"))
+for game in elements:
+    print("| {:<{}s} | {:<{}s} |".format(game['name'], WIDTH_VARIABLE, game['price'], WIDTH_VARIABLE))
+    # print("| {:<10s} | {:>7s}".format((game['name']), game['price']))
 
-response = requests.get(url)
-
-with open("output.html", 'w') as file:
-    data = response.json()
-    print(data.keys())
-    print(f"\n==================\n")
-    games = data['specials']['items']
-    output = []
-
-    print("| {:<50s} | {:<50s} |".format("NAME", "PRICE"))
-    print(60 *"-")
-    for game in games:
-        del game['large_capsule_image']
-        del game['small_capsule_image']
-        del game['windows_available']
-        del game['mac_available']
-        del game['linux_available']
-        del game['streamingvideo_available']
-        del game['header_image']
-        del game['type']
-        print("| {:50s} | {:>7s}".format((game['name']), format_price(game['final_price'])))
-
-    print(f"\n===================================\n")
+print(f"\n===================================\n")
