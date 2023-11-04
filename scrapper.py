3da557bca10393f65f003a60374c91c48e516ec3
MichalPrzyl <michal.przyl@gmail.com>
2023-11-04 15:13:43 +0100

    v 1.0.0

--- scrapper.py
+++ scrapper.py
@@ -1,38 +1,39 @@
 import requests
-from utils import format_price, color
+from utils import format_price
 
-
+# url found in google in 30 secs
 url = "http://store.steampowered.com/api/featuredcategories/?l=polish"
 response = requests.get(url)
 
-with open("output.html", 'w') as file:
-    data = response.json()
-    games = data['specials']['items']
-    MAX_NAME_LENGTH = max(len(d["name"]) for d in games)
-    MAX_PRICE_LENGTH = max(len(str(d["final_price"])) for d in games)
-    COLUMN_WIDTH_NAME= MAX_NAME_LENGTH
-    COLUMN_WIDTH_PRICE = MAX_PRICE_LENGTH + 4
-    WHOLE_WIDTH = COLUMN_WIDTH_NAME + COLUMN_WIDTH_PRICE + 7
+data = response.json()
+games = data['specials']['items']
 
-    # print headers
-    print(WHOLE_WIDTH * "=")
+MAX_NAME_LENGTH = max(len(d["name"]) for d in games)  # get the longest name
+MAX_PRICE_LENGTH = max(len(str(d["final_price"])) for d in games)  # get the longest price
+COLUMN_WIDTH_NAME= MAX_NAME_LENGTH
+COLUMN_WIDTH_PRICE = MAX_PRICE_LENGTH + 4
+WHOLE_WIDTH = COLUMN_WIDTH_NAME + COLUMN_WIDTH_PRICE + 7
 
-    print("| {:^{}s} | {:^{}s} |".format("NAME", COLUMN_WIDTH_NAME, "PRICE", COLUMN_WIDTH_PRICE))
-    print(WHOLE_WIDTH * "=")
-    for game in games:
-        del game['large_capsule_image']
-        del game['small_capsule_image']
-        del game['windows_available']
-        del game['mac_available']
-        del game['linux_available']
-        del game['streamingvideo_available']
-        del game['header_image']
-        del game['type']
-        print("| {:<{}s} | {:>{}s} |".format(
-                                            game['name'], 
-                                            COLUMN_WIDTH_NAME, 
-                                            format_price(game['final_price']) + " zł", 
-                                            COLUMN_WIDTH_PRICE))
+# print headers
+print(WHOLE_WIDTH * "=")
+print("| {:^{}s} | {:^{}s} |".format("NAME", COLUMN_WIDTH_NAME, "PRICE", COLUMN_WIDTH_PRICE))
+print(WHOLE_WIDTH * "=")
 
-    print(WHOLE_WIDTH * "=")
-    
+# removing unnecesary informations
+for game in games:
+    del game['large_capsule_image']
+    del game['small_capsule_image']
+    del game['windows_available']
+    del game['mac_available']
+    del game['linux_available']
+    del game['streamingvideo_available']
+    del game['header_image']
+    del game['type']
+    # actuall printing record (table look-like)
+    print("| {:<{}s} | {:>{}s} |".format(
+                                        game['name'], 
+                                        COLUMN_WIDTH_NAME, 
+                                        format_price(game['final_price']) + " zł", 
+                                        COLUMN_WIDTH_PRICE))
+
+print(WHOLE_WIDTH * "=")
